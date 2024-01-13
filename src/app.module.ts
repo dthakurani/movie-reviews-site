5b780a671e8aa9b74d4e29e7371809cdc09bdfeb
dthakurani <divyanshi.thakurani@gmail.com>
2024-01-13 10:01:22 +0530

    chore: configure database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: ['dist/entities/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Use 'combined' format to log detailed information about each request
+    const morganMiddleware = morgan('dev');
+
+    // Apply the morgan middleware to all routes
+    consumer.apply(morganMiddleware).forRoutes('*');
+  }
+}
